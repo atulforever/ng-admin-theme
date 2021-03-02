4958b4e8b0c5a588d00bd1eb1efd2a9b6af8d068
Atul Savaliya <savaliya.atul@ishanitech.biz>
2021-03-02 15:34:13 +0530

    Initialize Theme

---
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -0,0 +1,65 @@
+import {Injectable} from '@angular/core';
+
+export interface NavigationItem {
+  id: string;
+  title: string;
+  type: 'item' | 'collapse' | 'group';
+  translate?: string;
+  icon?: string;
+  hidden?: boolean;
+  url?: string;
+  classes?: string;
+  exactMatch?: boolean;
+  external?: boolean;
+  target?: boolean;
+  breadcrumbs?: boolean;
+  function?: any;
+  badge?: {
+    title?: string;
+    type?: string;
+  };
+  children?: Navigation[];
+}
+
+export interface Navigation extends NavigationItem {
+  children?: NavigationItem[];
+}
+
+const NavigationItems = [
+  {
+    id: 'navigation',
+    title: 'Navigation',
+    type: 'group',
+    icon: 'feather icon-monitor',
+    children: [
+      {
+        id: 'dashboard',
+        title: 'Dashboard',
+        icon: 'feather icon-home',
+        type: 'item',
+        url: '/dashboard/default'
+      },
+      {
+        id: 'page_custom',
+        title: 'Pages',
+        type: 'collapse',
+        icon: 'feather icon-home',
+        children: [
+          {
+            id: 'default',
+            title: 'Page1',
+            type: 'item',
+            url: '/page/page1'
+          }
+        ]
+      }
+    ]
+  }
+];
+
+@Injectable()
+export class NavigationItem {
+  public get() {
+    return NavigationItems;
+  }
+}
